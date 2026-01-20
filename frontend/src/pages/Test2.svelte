2f74479da429f4039bcfce18ff3fff8d97b93a04
Nico Grimm <nico.grimm@axelspringer.com>
2026-01-20 12:35:00 +0100

    Frontend: Datatable hinzufügen

--- frontend/src/pages/Test2.svelte
+++ frontend/src/pages/Test2.svelte
@@ -1,3 +1,40 @@
-<h2>Test2 page</h2>
+<script lang="ts">
+import { useAlerts } from "$lib/alerts";
+import Alertbox from "$lib/components/Alertbox.svelte";
+import { Button } from "$lib/components/ui/button";
 
-<a href="/" class="underline">Go to home</a>
+let testMsg: string | undefined;
+
+async function fetchTest() {
+  try {
+    let resp = await fetch("http://localhost:8080/test");
+
+    testMsg = await resp.text();
+  } catch (e) {
+    let { addAlert } = useAlerts();
+
+    addAlert({
+      level: "error",
+      title: "Server hat eine unerwartet Antwort gegeben",
+    });
+  }
+}
+</script>
+
+<main
+  class="m-w-screen m-0 flex min-h-screen flex-col place-items-center bg-white text-slate-900"
+>
+  <h2>Test2 page</h2>
+
+  <a href="/" class="underline">Go to home</a>
+
+  <div class="gap-2">
+    <Alertbox />
+
+    <Button onclick={fetchTest}>Test</Button>
+
+    {#if testMsg != undefined}
+      <p>{testMsg}</p>
+    {/if}
+  </div>
+</main>
