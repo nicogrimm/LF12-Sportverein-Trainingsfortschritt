59c007a13a70ae5b851c14f768d56bb49882d091
Nico Grimm <nico.grimm@axelspringer.com>
2026-01-21 09:44:51 +0100

    backend fix: postgresql Treiber wieder hinzufügen

--- backend/src/main/java/com/github/nicogrimm/LF12_Sportverein_Trainingsfortschritt/backend/repositories/AthleteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.nicogrimm.LF12_Sportverein_Trainingsfortschritt.backend.repositories;
-
-import com.github.nicogrimm.LF12_Sportverein_Trainingsfortschritt.backend.entities.Athlete;
-import lombok.NonNull;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AthleteRepository extends CrudRepository<@NonNull Athlete, @NonNull Integer> {
-
-}
