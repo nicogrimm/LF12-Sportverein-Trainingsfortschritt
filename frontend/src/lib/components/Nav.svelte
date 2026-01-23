9eaa7d15f82e90c92a622f41f0f266790be7526c
Nico Grimm <nico.grimm@axelspringer.com>
2026-01-23 09:02:40 +0100

    frontend feat: nav durch Tableiste

---
+++ frontend/src/lib/components/Nav.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+import * as Tabs from "$lib/components/ui/tabs/index.js";
+import { location, switchLocation, type Location } from "$lib/location";
+
+type Page = Extract<Location["page"], "sports" | "athletes" | "test2">;
+</script>
+
+<Tabs.Root
+  value={$location.page}
+  onValueChange={(page) => {
+  switchLocation({ page: page as Page })
+}}
+>
+  <Tabs.List>
+    <Tabs.Trigger value="athletes">Athleten</Tabs.Trigger>
+    <Tabs.Trigger value="sports">Sportarten</Tabs.Trigger>
+    <Tabs.Trigger value="test2">Test Seite</Tabs.Trigger>
+  </Tabs.List>
+</Tabs.Root>
